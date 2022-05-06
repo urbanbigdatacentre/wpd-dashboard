aeed27fdf64a7427a741d9b230a9f66c23a663bf
andyclarkemedia <andyclarkemedia@gmail.com>
2022-05-06 17:45:50 +0100

    Work on Sticky Control Panel on Scroll

---
+++ components/elements/controlPanelNav.js
@@ -0,0 +1,104 @@
+// Navigation Component for Control Panel - Will Stick to Top on Scroll
+
+// Package Imports
+import {connect} from "react-redux";
+import styles from "../../styles/modules/location-page/ControlPanel.module.css";
+import {Box, Container, Toolbar} from "@mui/material";
+import MyButton from "./button";
+import uiText from "../../data/ui-text";
+import LocationControlButton from "./locationControlButton";
+import {locationColorKeys} from "../../data/colorMapping";
+import DateFilter from "./dateFilter";
+import {useEffect, useState} from "react";
+
+// Local Imports
+
+
+// Control Panel Nav Component
+
+const ControlPanelNav = ({toggleLanguage, updateAdditionalLocation, updatePrimaryLocation, clickHandler, setAddingLocationStatus }) => {
+
+    const [sticky, setSticky] = useState(false);
+
+    useEffect(() => {
+        const windowOverlay = document.querySelector('.window-overlay')
+        if (windowOverlay) { windowOverlay.addEventListener('click', function() {setAddingLocationStatus(false);})}
+
+        window.onscroll = function() {
+            setSticky(document.querySelector('#control-panel').getBoundingClientRect().top < 64)
+        }
+
+    }, [sticky])
+
+    const result = sticky ? (
+        <Box>
+            <Box id={'control-panel'}>
+                <Box className={styles.controlPanelInnerBoxSticky} sx={{}}>
+                    <Container>
+                        <Box className={styles.locationControlBox}>
+                            <MyButton text={uiText.global.labels.addLocation[toggleLanguage.language]} variant={"contained"} onClick={clickHandler}/>
+                            <Box sx={{marginLeft: `40px`, marginRight: `40px`}}>
+                                <LocationControlButton primary={true} data={updatePrimaryLocation.location} contained={false} color={'#2196F3'}/>
+                            </Box>
+                            {/*SPACE HERE TO MAP OTHER CONTROL BUTTONS*/
+                                updateAdditionalLocation.locations.length ? updateAdditionalLocation.locations.map((item, index) => {
+                                    return (
+                                        <Box sx={{marginRight: `40px`, }} key={index}>
+                                            <LocationControlButton data={item} contained={false} color={locationColorKeys[index].color}/>
+                                        </Box>
+                                    )
+                                }) : null
+                            }
+                        </Box>
+                        <DateFilter positionAbsolute={false}/>
+                    </Container>
+                </Box>
+            </Box>
+            <div>
+                <Box className={styles.controlPanelInnerBoxHolder}>
+                    <Box className={styles.locationControlBox}>
+                        <MyButton text={uiText.global.labels.addLocation[toggleLanguage.language]} variant={"contained"} onClick={clickHandler}/>
+                        <Box sx={{marginLeft: `40px`, marginRight: `40px`}}>
+                            <LocationControlButton primary={true} data={updatePrimaryLocation.location} contained={false} color={'#2196F3'}/>
+                        </Box>
+                        {/*SPACE HERE TO MAP OTHER CONTROL BUTTONS*/
+                            updateAdditionalLocation.locations.length ? updateAdditionalLocation.locations.map((item, index) => {
+                                return (
+                                    <Box sx={{marginRight: `40px`, }} key={index}>
+                                        <LocationControlButton data={item} contained={false} color={locationColorKeys[index].color}/>
+                                    </Box>
+                                )
+                            }) : null
+                        }
+                    </Box>
+                    <DateFilter positionAbsolute={false}/>
+                </Box>
+            </div>
+        </Box>
+    ) : (
+        <div id={'control-panel'}>
+            <Box className={styles.controlPanelInnerBox}>
+                <Box className={styles.locationControlBox}>
+                    <MyButton text={uiText.global.labels.addLocation[toggleLanguage.language]} variant={"contained"} onClick={clickHandler}/>
+                    <Box sx={{marginLeft: `40px`, marginRight: `40px`}}>
+                        <LocationControlButton primary={true} data={updatePrimaryLocation.location} contained={false} color={'#2196F3'}/>
+                    </Box>
+                    {/*SPACE HERE TO MAP OTHER CONTROL BUTTONS*/
+                        updateAdditionalLocation.locations.length ? updateAdditionalLocation.locations.map((item, index) => {
+                            return (
+                                <Box sx={{marginRight: `40px`, }} key={index}>
+                                    <LocationControlButton data={item} contained={false} color={locationColorKeys[index].color}/>
+                                </Box>
+                            )
+                        }) : null
+                    }
+                </Box>
+                <DateFilter positionAbsolute={false}/>
+            </Box>
+        </div>
+    )
+
+    return result;
+}
+
+export default connect((state) => state)(ControlPanelNav)
--- components/modules/location-page/controlPanel.js
+++ components/modules/location-page/controlPanel.js
@@ -13,6 +13,7 @@ import uiText from "../../../data/ui-text";
 import MyButton from "../../elements/button";
 import LocationControlButton from "../../elements/locationControlButton";
 import ControlDashboard from "../../elements/controlDashboard";
+import ControlPanelNav from "../../elements/controlPanelNav";
 import {locationColorKeys} from "../../../data/colorMapping";
 import AddingLocationWindow from "../../elements/addingLocationWindow";
 
@@ -24,17 +25,6 @@ import styles from '../../../styles/modules/location-page/ControlPanel.module.cs
 const ControlPanel = ({ toggleLanguage, updatePrimaryLocation, updateAdditionalLocation }) => {
 
     const [addingLocationStatus, setAddingLocationStatus] = useState(false);
-    const [sticky, setSticky] = useState(false);
-
-    useEffect(() => {
-        const windowOverlay = document.querySelector('.window-overlay')
-        if (windowOverlay) { windowOverlay.addEventListener('click', function() {setAddingLocationStatus(false);})}
-
-        window.onscroll = function() {
-            setSticky(document.querySelector('#control-panel').getBoundingClientRect().top < 64)
-        }
-
-    }, [sticky])
 
     const handleClick = (e) => {
         setAddingLocationStatus(true);
@@ -57,24 +47,7 @@ const ControlPanel = ({ toggleLanguage, updatePrimaryLocation, updateAdditionalL
             {
                 // INSERT LOGIC TO APPEND CONTROL PANEL ON SCROLL
 
-            <Box id={'control-panel'} className={styles.controlPanelInnerBox}>
-                <Box className={sticky ? styles.locationControlBoxSticky : styles.locationControlBox}>
-                    <MyButton text={uiText.global.labels.addLocation[toggleLanguage.language]} variant={"contained"} onClick={handleClick}/>
-                    <Box sx={{marginLeft: `40px`, marginRight: `40px`}}>
-                        <LocationControlButton primary={true} data={updatePrimaryLocation.location} contained={false} color={'#2196F3'}/>
-                    </Box>
-                    {/*SPACE HERE TO MAP OTHER CONTROL BUTTONS*/
-                        updateAdditionalLocation.locations.length ? updateAdditionalLocation.locations.map((item, index) => {
-                            return (
-                                <Box sx={{marginRight: `40px`, }} key={index}>
-                                    <LocationControlButton data={item} contained={false} color={locationColorKeys[index].color}/>
-                                </Box>
-                            )
-                        }) : null
-                    }
-                </Box>
-                <DateFilter positionAbsolute={false}/>
-            </Box>
+            <ControlPanelNav clickHandler={handleClick} setAddingLocationStatus={setAddingLocationStatus}/>
 
             }
             <ControlDashboard color={'#2196F3'} locationData={updatePrimaryLocation.location}/>
